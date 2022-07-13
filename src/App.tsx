47717741010d30ef53d71bd93ade14e8d56e16f0
Jordan Skomal <jskomal@gmail.com>
2022-07-13 04:12:43 -0600

    Set up end checking logic

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,34 @@ import { useState } from 'react'
 import Header from './components/Header'
 import GameBoard from './components/GameBoard'
 
+import { WINNING_COMBINATIONS } from './data/utils'
+
 const App = () => {
   const [score, setScore] = useState([0, 0])
+  const [isFirstPlayerTurn, setIsFirstPlayerTurn] = useState(true)
+  const [firstPlayerPositions, setFirstPlayerPositions] = useState<Number[]>([])
+  const [secondPlayerPositions, setSecondPlayerPositions] = useState<Number[]>([])
+  const [isGameEnded, setIsGameEnded] = useState(false)
+
+  const changeTurn = () => {
+    setIsFirstPlayerTurn((prev) => !prev)
+  }
+
+  const checkForEnd = () => {
+    WINNING_COMBINATIONS.forEach((combination) => {
+      if (combination.every((number) => firstPlayerPositions.includes(number))) {
+        setScore((prev) => {
+          prev[0]++
+          return [...prev]
+        })
+      } else if (combination.every((number) => secondPlayerPositions.includes(number))) {
+        setScore((prev) => {
+          prev[1]++
+          return [...prev]
+        })
+      }
+    })
+  }
 
   return (
     <div className='App'>
