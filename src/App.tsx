9b2fca9bf41987515df6d85a449dae43f30e1c8e
Jordan Skomal <jskomal@gmail.com>
2022-07-13 03:55:43 -0600

    Clean up app and create components

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+import Header from './components/Header'
+import GameBoard from './components/GameBoard'
+
+const App = () => {
+  const [score, setScore] = useState([0, 0])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Header />
+      <GameBoard />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
