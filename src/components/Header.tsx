e6706a2b691f500b1ea65ce311007348fe436ddf
Jordan Skomal <jskomal@gmail.com>
2022-07-18 02:46:27 -0600

    Add Player scores to Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
-const Header = () => {
+type HeaderProps = {
+  score: number[]
+}
+
+const Header = ({ score }: HeaderProps) => {
   return (
     <div>
       <h1>TIC TAC TOE</h1>
+      <span>Player 1: {score[0]} | </span>
+      <span>Player 2: {score[1]}</span>
     </div>
   )
 }
