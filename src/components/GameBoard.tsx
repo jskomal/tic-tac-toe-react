80d00ee2b245f0940e2d2afb234d762d728635fb
Jordan Skomal <jskomal@gmail.com>
2022-07-18 02:47:17 -0600

    Implement Game Logic on click

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,20 +1,42 @@
-import React, { useState, useRef } from 'react'
+type GameBoardProps = {
+  placeToken: (position: number) => void
+  board: string[]
+}
 
-type GameBoardProps = {}
-
-const GameBoard = (props: GameBoardProps) => {
+const GameBoard = ({ placeToken, board }: GameBoardProps) => {
   return (
     <div className='game-view'>
-      <section className='board'>
-        <div className='tic'>1</div>
-        <div className='tic'>2</div>
-        <div className='tic'>3</div>
-        <div className='tic'>4</div>
-        <div className='tic'>5</div>
-        <div className='tic'>6</div>
-        <div className='tic'>7</div>
-        <div className='tic'>8</div>
-        <div className='tic'>9</div>
+      <section
+        onClick={(e) => placeToken(parseInt((e.target as Element).id))}
+        className='board'
+      >
+        <div className='tic' id='0'>
+          {board[0]}
+        </div>
+        <div className='tic' id='1'>
+          {board[1]}
+        </div>
+        <div className='tic' id='2'>
+          {board[2]}
+        </div>
+        <div className='tic' id='3'>
+          {board[3]}
+        </div>
+        <div className='tic' id='4'>
+          {board[4]}
+        </div>
+        <div className='tic' id='5'>
+          {board[5]}
+        </div>
+        <div className='tic' id='6'>
+          {board[6]}
+        </div>
+        <div className='tic' id='7'>
+          {board[7]}
+        </div>
+        <div className='tic' id='8'>
+          {board[8]}
+        </div>
       </section>
     </div>
   )
