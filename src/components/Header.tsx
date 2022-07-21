6f7c8eb6cbed57efd27fb17db875fc290f2c02a1
Jordan Skomal <jskomal@gmail.com>
2022-07-20 20:49:43 -0600

    Conditionally render Player Turn

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 
 type HeaderProps = {
   score: number[]
+  isFirstPlayerTurn: boolean
 }
 
-const Header = ({ score }: HeaderProps) => {
+const Header = ({ score, isFirstPlayerTurn }: HeaderProps) => {
   return (
     <div>
       <h1>TIC TAC TOE</h1>
       <span>Player 1: {score[0]} | </span>
       <span>Player 2: {score[1]}</span>
+      <p>Player {isFirstPlayerTurn ? '1' : '2'}, it's your turn!</p>
     </div>
   )
 }
